18aec537f3756a31ca2883a80556b75125363dac
Xiaodong <xiaoyan@microsoft.com>
2022-12-08 12:42:55 +0000

    saving

--- python/README.md
+++ python/README.md
@@ -8,10 +8,11 @@ az login
 az account set --subscription c830bb7a-83f5-45e3-81fc-3c2053e7d16f  
 az acr login --name xiaoyanacr  
 
-docker build . -f Dockerfile -t xiaoyanacr.azurecr.io/test/aml-pytest:1130-1
-docker run -p 8000:8000  xiaoyanacr.azurecr.io/test/aml-pytest:1130-1
-docker push  xiaoyanacr.azurecr.io/test/aml-pytest:1130-1
+docker build . -f Dockerfile -t xiaoyanacr.azurecr.io/test/aml-pytest:1208-1
+docker run -p 8000:8000  xiaoyanacr.azurecr.io/test/aml-pytest:1208-1
+docker push  xiaoyanacr.azurecr.io/test/aml-pytest:1208-1
 
 az ml online-endpoint create  -f pytest-endpoint.yml --resource-group xiaoyan-group-dev --workspace-name xiaoyan-aml-ws  
 az ml online-deployment create  -f pytest-deployment.yml --all-traffic --resource-group xiaoyan-group-dev --workspace-name xiaoyan-aml-ws  
 
+az ml online-endpoint get-credentials --name pytest-endpoint1  --resource-group xiaoyan-group-dev --workspace-name xiaoyan-aml-ws
--- python/src/main.py
+++ python/src/main.py
@@ -5,7 +5,9 @@ import os
 from flask import Flask, jsonify, request
 from azure.identity import ManagedIdentityCredential
 from azure.eventhub import EventHubProducerClient, EventData, EventDataBatch
+from pyraisdk import rlog
 
+rlog.initialize(eh_structured='struct', eh_unstructured='unstruct', eh_hostname='xiaoyan-eh.servicebus.windows.net', client_id='9fd61a68-1faa-4bca-9a4e-8611b254d342')
 app = Flask(__name__)
 
 
@@ -17,21 +19,9 @@ def liveness():
 def readiness():
     return "ok"
 
-@app.route("/send")
-def send():
+@app.route("/score")
+def score():
     msg = request.args.get('msg')
-    send_to_eventhub('xiaoyan-eh.servicebus.windows.net', "9fd61a68-1faa-4bca-9a4e-8611b254d342", 'aml-py-test', msg)
+    rlog.infof(msg)
     return msg
-
-@app.route("/hostname")
-def hostname():
-    return os.uname()[1]
-
-def send_to_eventhub(hostname, client_id, ehname, msg):
-    credential = ManagedIdentityCredential(client_id=client_id)
-    client = EventHubProducerClient(hostname, ehname, credential)
-    batch = client.create_batch()
-    batch.add(EventData(msg))
-    client.send_batch(batch)
-
 
